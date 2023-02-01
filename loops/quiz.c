2b602276a28fa70202c6256bb050adaeef9d897c
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2023-02-01 19:44:27 +0530

    Loops in C

---
+++ loops/quiz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    while (i <= 20)
+    {
+        if (i >= 10)
+        {
+            printf("%d \n", i);
+        }
+        i++;
+    }
+    return 0;
+}
