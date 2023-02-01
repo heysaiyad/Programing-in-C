d6c54ca40b1735e8041e046c1343a1ed5e9ef66e
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2023-02-01 19:45:24 +0530

    Loops in C

---
+++ loops/whileLoops.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int i = 0;
+    while (i<5)
+    {
+        printf("Ali ");
+        i++;
+        // i = i+2;
+    }
+    
+}
+/*
+int main(){
+    int i = 0;
+    while(i<5); 
+    {
+        printf("Ali");
+        i++;
+    }
+    return 0;
+}*/
