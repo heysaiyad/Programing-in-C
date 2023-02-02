8340e21c0134d2fb9cc0241734f1dc96e21df7dc
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2023-02-02 19:30:24 +0530

    Function in C

---
+++ Function/greeting.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void greeting(){    
+    printf("Hi, \n");
+    printf("Welcome to Flexiui \n");
+}
+
+void exiting(){
+    printf("Bye \n");
+    printf("Visit again \n");
+}
+
+int main(){
+
+    greeting();
+    printf("Hope you are enjoying \n");
+    exiting();
+
+    
+    return 0;
+}
