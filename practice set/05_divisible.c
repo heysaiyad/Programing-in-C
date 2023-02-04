e83eebae880630f43154d0c845ea5245a68610bf
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2023-02-04 19:09:43 +0530

    Practice

---
+++ practice set/05_divisible.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a;
+    printf("Enter a number for divisible by 97 \n");
+    scanf("%d", &a);
+
+    printf("Divisiblity test return: %d", a%97);
+
+    
+    return 0;
+}
