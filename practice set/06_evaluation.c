f7fd87b79aa930c7bd966cb560c89486db3ac9c8
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2023-02-05 14:43:07 +0530

    Practice

---
+++ practice set/06_evaluation.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int x = 2;
+    int y = 3;
+    int z = 3;
+    int k = 1;
+
+    // 3*x/y-z+k
+
+    int result = 3*x /y - z + k;
+    // 6/3 - 3 +1
+    // 2 - 3 + 1
+    // 0
+
+    printf("the value of result is %d\n", result);
+    return 0;
+}
