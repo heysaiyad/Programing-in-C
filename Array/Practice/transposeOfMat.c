90e25c78de657525bcf32198695bf478fc8f341e
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2023-02-14 20:20:05 +0530

    Practice in Arrays

---
+++ Array/Practice/transposeOfMat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    int n = 4, temp; 
+    int arr[4][4] = {{1,1,1,1},{2,2,2,2},{3,3,3,3},{4,4,4,4}};
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         temp = arr[i][j];
+    //         arr[i][j] =arr[j][i];
+    //         arr[j][i] = temp;
+    //     }
+        
+    // }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", arr[j][i]);
+            
+        }
+        printf("\n");
+        
+    }
+    
+    
+
+    return 0;
+}
