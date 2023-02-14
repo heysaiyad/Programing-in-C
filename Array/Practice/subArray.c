a82c60963c3d43a017e7c24a23c31fef7a9eb82a
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2023-02-14 20:18:48 +0530

    Practice in Arrays

---
+++ Array/Practice/subArray.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int subAr(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                printf("%d ", arr[k]);
+            }
+            printf("\n");
+        }
+    }
+}
+int main()
+{
+    int arr[3] = {1, 2, 3};
+    subAr(arr, 3);
+    return 0;
+}
