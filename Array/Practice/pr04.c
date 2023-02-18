ecc0d994eca18f2913f8b5e98dade53d320f342d
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2023-02-19 02:08:15 +0530

    Practice with Array

---
+++ Array/Practice/pr04.c
@@ -0,0 +1,15 @@
+// Table of number
+#include <stdio.h>
+
+int main(){
+    
+    int n = 3;
+    int m = 4;
+    for (int i = 1; i <= m; i++)
+    {
+        int mul = n *i;
+        printf("%d \n", mul);
+    }
+    
+    return 0;
+}
