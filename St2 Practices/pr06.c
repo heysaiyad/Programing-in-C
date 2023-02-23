283dcb8d6379b98a33daaba9e3edcad2b34181a7
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2023-02-23 11:44:16 +0530

    Practice for st2

---
+++ St2 Practices/pr06.c
@@ -0,0 +1,28 @@
+// C program to find intersection of two sorted arrays
+#include <stdio.h>
+void printIntersection(int arr1[], int arr2[], int m, int n)
+{
+	int i = 0, j = 0;
+	while (i < m && j < n) {
+		if (arr1[i] < arr2[j])
+			i++;
+		else if (arr2[j] < arr1[i])
+			j++;
+		else /* if arr1[i] == arr2[j] */
+		{
+			printf(" %d ", arr2[j++]);
+			i++;
+		}
+	}
+}
+
+int main()
+{
+	int arr1[] = { 1, 2, 4, 5, 6 };
+	int arr2[] = { 2, 3, 5, 7 };
+	int m = sizeof(arr1) / sizeof(arr1[0]);
+	int n = sizeof(arr2) / sizeof(arr2[0]);
+	printIntersection(arr1, arr2, m, n);
+	getchar();
+	return 0;
+}
