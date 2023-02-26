b05c18cd1617fe51ead1c77c7d549e245be5f780
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2023-02-26 11:18:45 +0530

    Practice with Loops

---
+++ loops/Practice/pr01.c
@@ -0,0 +1,13 @@
+// print * * * *  * * * *  * * 
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("* ");
+    }
+    
+    return 0;
+}
